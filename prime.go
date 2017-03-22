7a73d745e97c97e0137a79f68a191ecd4f839f78
Nathan Miles <nmiles@ncsu.edu>
2017-03-22 00:14:17 -0400

    Switched to sync.WaitGroup for the exit trigger

--- prime.go
+++ prime.go
@@ -3,7 +3,11 @@ package main
 import (
     "fmt"
     "math"
+    "sync"
 )
+
+var wg sync.WaitGroup
+
 /*
  * Prints out all prime numbers under max, then exits
  */
@@ -12,32 +16,36 @@ func main() {
     firstChannel := make(chan int)
     resultChannel := make(chan int)
     go worker(2, int(math.Sqrt(float64(max))), firstChannel, resultChannel)
-    
-    endChannel := make(chan int) // this is used as a signal for when we're done
-    go printer(resultChannel, endChannel)  // need a printer goroutine so we don't deadlock
+    // workers feed the printer
+    go printer(resultChannel)
+    wg.Add(1)
     for i := 2; i <= max; i++ {
         firstChannel <- i;
     }
-    close(firstChannel) // tell the workers we're done
-    <- endChannel // wait for the printer to signal it's done printing
+    // tell the workers we're done
+    close(firstChannel)
+    // wait for the printer to finish printing all the numbers
+    wg.Wait()
 }
 
 /*
- * Reads values from result and prints them until it gets a value < 0, then it
- * signals end
+ * Reads values from result and prints them until it gets a value < 0, then
+ * signals main() and exits
  */
-func printer(result <-chan int, end chan<- int){
+func printer(result <-chan int){
     for i := range result {
         fmt.Println(i)
     }
-    end <- 0
+    wg.Done()
 }
 
 /*
  * One link in a long chain. Each link in the chain has base equal to a prime
  * number. Each link in the chain receives integers from myChannel and if that
  * number is not divisible by base, sends it to the next link in the chain. The
- * last link in the chain sends to the printer function, which prints it.
+ * last link in the chain sends to the printer function, which prints it. When
+ * a link's channel is closed, it then closes the next channel in the chain. The
+ * last one closes printer()'s channel, which triggers the program exit.
  */
 func worker(base, end int, myChannel chan int, resultChannel chan int){
     var nextChannel chan int
